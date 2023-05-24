e03678ed477975b744b6f7ac45810ba9ed3d39d0
DKD Support Team <support@updcon.net>
2023-05-24 23:51:17 +0300

    initial

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='py-consul-tiny',
+      version='0.1',
+      description='The tiny Consul client for python 3.x',
+      url='https://github.com/dkdhub/py-consul-tiny',
+      author='UPDCon',
+      author_email='support@updcon.net',
+      license='MIT',
+      packages=['consul'],
+      install_requires=['requests'])
